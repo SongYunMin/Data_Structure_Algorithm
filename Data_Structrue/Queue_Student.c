e7494cbafcc599353fbd893ce31dc2f6bf45d0df
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-05-27 15:49:28 +0900

    Queue_Linked_List

--- Data_Structrue/Queue_Student.c
+++ Data_Structrue/Queue_Student.c
@@ -1,25 +1,17 @@
-///*
-//DATA
-//56
-//45
-//3
-//46
-//57
-//12
-//74
-//*/
 //#include <stdio.h>
 //#include <stdlib.h>
-//#include <string.h>
+//
 //#define MAX_QUEUE_SIZE 100
 //#define TRUE 1
 //#define FALSE 0
 //
-//typedef int QueueObject;
+//typedef struct QueueObject {
+//	int nNum;
+//	char Name[100];
+//} QueueObject;
+//
 //QueueObject queue[MAX_QUEUE_SIZE];
 //int front, rear;
-//
-//
 //// 큐 초기화 함수
 //void initialize(void)
 //{
@@ -64,7 +56,7 @@
 //	if (isEmpty())
 //	{
 //		printf("Queue is Empty\n");
-//		return -1;
+//		exit(1);
 //	}
 //
 //	front = (front + 1) % MAX_QUEUE_SIZE;
@@ -75,31 +67,32 @@
 //int main(void)
 //{
 //	FILE* fp;
-//	int temp;
-//	int nCount, i;
+//	int nCount;
+//	QueueObject temp;			// fscanf시 파일을 읽어야 하기 때문에 temp 구조체 필요
+//	fp = fopen("data.txt", "r");
 //
-//	fp = fopen("data.txt", "rt");
 //	if (fp == NULL)
 //	{
 //		printf("File Not Found\n");
 //		return 0;
 //	}
-//
-//	// read the file and insert into the Queue
 //	nCount = 0;
 //	while (!feof(fp))
 //	{
-//		fscanf(fp, "%d",&temp);
+//		fscanf(fp, "%d %s", &temp.nNum, temp.Name);
 //		addQ(temp);
 //		nCount++;
 //	}
 //
-//	printf("front = %d, rear = %d\n", front, rear);
-//	for (i = 0; i < nCount; i++)
+//	printf("front=%d rear=%d\n", front, rear);
+//
+//	for (int i = 0; i < nCount; i++)
 //	{
-//		printf("deleteQ() = %d\n", deleteQ());
+//		temp = deleteQ();
+//		printf("%d | %s\n", temp.nNum, temp.Name);
 //	}
-//	printf("front %d rear %d\n",front,rear);
+//
+//	printf("front=%d rear=%d\n", front, rear);
 //
 //	fclose(fp);
 //	return 0;
